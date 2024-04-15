6e205a2859325d67fdd305cb586ca4dd3232c2bd
morendong <45411847+853924164@users.noreply.github.com>
2024-04-16 00:29:36 +0800

    测试提交代码

--- src/main/java/cn/lwapi/Test.java
+++ src/main/java/cn/lwapi/Test.java
@@ -4,5 +4,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println(11111);
+
+        // 测试上传
+        String userDir = System.getProperty("user.dir");
+        System.out.println(userDir);
     }
 }
